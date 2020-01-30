edcaa3de0618dc1e0138c2bb5e816683f1784a25
gizgs <arthur.giza@gmail.com>
2020-01-30 23:59:03 +0100

    raz

--- movierater/settings.py
+++ movierater/settings.py
@@ -118,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['moje_static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'moje_media'
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = ''
 
